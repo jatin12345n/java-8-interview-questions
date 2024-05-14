cba0298e6df18b07aafc3569a620565e87ef95ed
jatin12345n <jitinshakya786jks@gmail.com>
2024-05-14 14:57:47 +0530

    Employee, Student

---
+++ src/java_8_interview_programme/model/Employee.java
@@ -0,0 +1,50 @@
+package java_8_interview_programme.model;
+
+import java.text.DecimalFormat;
+
+public class Employee {
+
+    private String name;
+    private Integer age;
+    private  Double salary;
+
+
+    public Employee(String name, Integer age, Double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+    //Getters and Setters of name, age & salary go here
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+    public String toString(){
+        DecimalFormat decimalFormat= new DecimalFormat(".##");
+        return "Employee Name:"+this.name
+                + " Age:"+this.age
+                + " Salary:"+decimalFormat.format(this.salary);
+    }
+    //Standard equals() and hashcode() implementations go here
+}
